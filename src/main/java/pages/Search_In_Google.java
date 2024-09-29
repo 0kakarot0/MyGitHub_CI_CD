a14f96bb1c8a039f6aafe4edfc54f59366fb1210
Ahtisham <ilyasahtsham@gmail.com>
2024-09-29 22:33:49 +0500

    added locator and method to click on the googleImage

--- src/main/java/pages/Search_In_Google.java
+++ src/main/java/pages/Search_In_Google.java
@@ -2,13 +2,12 @@ package pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.support.locators.RelativeLocator;
 import utils.SeleniumUtils;
 
 public class Search_In_Google extends SeleniumUtils {
     private final By searchFieldLocator = By.name("q");
-    private final By googleSearchButtonLocator
-            = RelativeLocator.with(By.xpath("//input[@value=\"Google Search\"]")).above(By.xpath("//input[@value=\"I'm Feeling Lucky\"]"));
+    private final By googleImageLocator = By.xpath("/html/body/div[1]/div[2]/div/img");
+    private final By googleSearchButtonLocator = By.xpath("(//*[@value=\"Google Search\" and @type=\"submit\"])[2]");
 
     public Search_In_Google(WebDriver driver) {
         super(driver);
@@ -18,6 +17,11 @@ public class Search_In_Google extends SeleniumUtils {
         waitForWebElementToBeClickAble(searchFieldLocator);
         clickOnWebElement(searchFieldLocator);
         enterValueInWebElement(searchFieldLocator, searchQuery);
+    }
+
+    public void clickOnGoogleImage() {
+        waitForWebElementToBeClickAble(googleImageLocator);
+        clickOnWebElement(googleImageLocator);
     }
 
     public void clickOnSearchButton() {
